488eb85573f105c5a028c66d1ee46201d5ad3992
alinab2829 <alinaboltaeva134@gmail.com>
2025-10-24 20:45:51 +1000

    Add files via upload

---
+++ phone/src/main/java/todo/Main.java
@@ -0,0 +1,114 @@
+package todo;
+// все что начинается с Javafx то это для графического дизайна
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.stream.Collectors;
+//создание кнопок окон список задач
+public class Main extends Application{
+    private final LinkedTasks model  = new LinkedTasks();//model - хранит все данные
+    private final ObservableList<String> uiItems = FXCollections.observableArrayList();
+
+    private ListView<String> listView;
+    private TextField input_name;
+    private TextField input_num;
+    private Label status;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Телефонная книжка");
+        input_name= new TextField();
+        input_name.setPromptText("Новый контакт");
+        input_num= new TextField();
+        input_num.setPromptText("Телефон");
+        Button addBtn = new Button("Добавить");
+        addBtn.setDefaultButton(true);
+
+        HBox inputBox = new HBox(8, input_name,input_num, addBtn);
+        inputBox.setAlignment(Pos.CENTER_LEFT);
+
+        listView = new ListView<>(uiItems);
+        listView.setPrefHeight(420);
+
+//        Button doneBtn = new Button("Готово");
+        Button delBtn = new Button("Удалить");
+        Button upBtn = new Button("⬆");
+        Button dnBtn = new Button("⬇");
+        Button clrBtn = new Button("Очистить");
+
+        HBox action = new HBox(8, delBtn, upBtn, dnBtn, clrBtn);
+        action.setAlignment(Pos.CENTER_LEFT);
+
+        status = new Label("Готово");
+        VBox root = new VBox(12, new Label("Список контактов"), inputBox, listView, action, status);
+        root.setPadding(new Insets(16));
+
+//        обработчики
+        addBtn.setOnAction(e-> addTask());
+        root.setOnKeyPressed(e -> {if (e.getCode()== KeyCode.ENTER) addTask();});
+
+//        doneBtn.setOnAction(e-> {
+//           int i = listView.getSelectionModel().getSelectedIndex();
+//           model.toogleAt(i);
+//           syncUi();
+//        });
+
+        delBtn.setOnAction(e->{
+            int i = listView.getSelectionModel().getSelectedIndex();
+            model.removeAt(i);
+            syncUi();
+        });
+
+        upBtn.setOnAction(e -> move(-1));
+        dnBtn.setOnAction(e -> move(+1));
+        clrBtn.setOnAction(e -> {model.clean(); syncUi();});
+
+//        Демо данные
+        model.addLast("Новый контакт");
+        syncUi();
+
+        stage.setScene(new Scene(root, 640, 520));
+        stage.show();
+
+
+    }
+
+    private void addTask() {
+        String text = input_name.getText().trim();
+        String text1 = input_num.getText().trim();
+        if(text.isEmpty()){status.setText("Введите текст задачи");}
+        model.addLast("Имя: " + text + "\n номер: " + text1);
+        input_name.clear();
+        input_num.clear();
+        syncUi();
+        status.setText("Добавлено: \"" +"Имя: " + text  +" номер: " + text1 +"\"" );
+    }
+
+    private void move(int delta){
+        int i = listView.getSelectionModel().getSelectedIndex();
+        model.move(i, delta);
+        syncUi();
+        int target = Math.max(0, Math.min(i + delta, model.size() - 1));
+        listView.getSelectionModel().select(target);
+    }
+//мост между логичкой и экраном
+    private void syncUi() {
+        uiItems.setAll(model.asList().stream().map(Task::toString).collect(Collectors.toList()));
+        status.setText("Всего контактов: " + model.size());
+    }
+}
---
+++ phone/src/main/java/todo/Task.java
@@ -0,0 +1,18 @@
+package todo;
+//модель задачи для демонстрации LinkedTastk
+public class Task {
+    public String title;
+    public boolean done;
+
+    public Task(String title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    @Override
+    //переопределение метода из класса object
+    public String toString (){
+        // отображение списков
+        return (done ? "✔":"📞") + title;
+    }
+}
